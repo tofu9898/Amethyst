3d615c8b5d22a6fe8c332095432340769ffce676
tofu9898 <rychtarechpatrik123@gmail.com>
2025-02-16 18:27:19 +0100

    nevim

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Horizontal Movement Settings")]
+    [SerializeField] private float walkSpeed = 1;
+
+    [Header("Ground Check Settings")]
+    [SerializeField] private float jumpForce = 30;
+    [SerializeField] private Transform groundCheckPoint;
+    [SerializeField] private float groundCheckY = 0.2f;
+    [SerializeField] private float groundCheckX = 0.5f;
+    [SerializeField] private LayerMask whatIsGround;
+
+    private Rigidbody2D rb;
+    private float xAxis;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInputs();
+        Move();
+        Jump();
+    }
+
+    void GetInputs()
+    {
+        xAxis = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void Move()
+    {
+        rb.linearVelocity = new Vector2(walkSpeed * xAxis, rb.linearVelocity.y);
+    }
+
+    public bool Grounded()
+    {
+        if (Physics2D.Raycast(groundCheckPoint.position, Vector2.down, groundCheckY, whatIsGround)
+            || Physics2D.Raycast(groundCheckPoint.position + new Vector3(groundCheckX, 0, 0), Vector2.down, groundCheckY, whatIsGround)
+            || Physics2D.Raycast(groundCheckPoint.position + new Vector3(-groundCheckX, 0, 0), Vector2.down, groundCheckY, whatIsGround))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && Grounded())
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce);
+        }
+    }
+}
